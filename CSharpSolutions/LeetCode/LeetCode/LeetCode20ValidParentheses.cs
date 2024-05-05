f89e7243eacdababa426181fc834d1852ae4ecea
Justin <jmms5757@gmail.com>
2024-05-05 14:22:10 -0400

    Created CSharp folder and completed 20

---
+++ CSharpSolutions/LeetCode/LeetCode/LeetCode20ValidParentheses.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode;
+
+public static class LeetCode20ValidParentheses {
+    public static bool IsValid(string s) {
+        if (s.Length % 2 != 0) return false;
+        
+        var stack = new Stack<char>();
+
+        foreach (char c in s) {
+            if (c is '(' or '[' or '{') {
+                stack.Push(c);
+            } else {
+                if (stack.Count == 0) {
+                    return false;
+                }
+
+                char first = stack.Pop();
+                switch (c) {
+                    case ')' when first != '(':
+                    case ']' when first != '[':
+                    case '}' when first != '{':
+                        return false;
+                }
+            }
+        }
+        
+        return stack.Count <= 0;
+    }
+}
---
+++ CSharpSolutions/LeetCode/LeetCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode;
+
+public static class Program {
+    public static void Main() {
+        bool state = LeetCode20ValidParentheses.IsValid("([)]");
+        
+        Console.WriteLine("Parentheses are valid: " + state);
+    }
+}
